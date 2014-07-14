005bc10dde068d58f448232913ee4ef0cee6c509
CTAPbIu_MABP <ctapbiumabp@gmail.com>
2014-07-14 13:11:22 +0300

    small fix for orbits

--- assets/js/solaris/models/orbit.js
+++ assets/js/solaris/models/orbit.js
@@ -17,11 +17,11 @@ define([
     _.extend(Orbit.prototype, {
         _url: "/ephemeris/getObjectOrbit",
         getPosition: function (params) {
-            var start = new Date(),
-                end = new Date(2230, 0, 1),
+            var end = new Date(), // 2214-10-21
+                start = new Date(1787, 8, 10), // 1787-09-10
                 diff = ~~((end - start) / 1000 / 60 / 60 / 24), // -1 ?
                 data = {
-                    date: start.toISOString().slice(0, 10),
+                    date: end.toISOString().slice(0, 10),
                     days: this._params.days < diff ? this._params.days : diff,
                     object: this._params.index,
                     step: this._params.step || 1,
