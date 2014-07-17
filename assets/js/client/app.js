883db06051fae584f51dc7a55c02adee58dff2f1
CTAPbIu_MABP <ctapbiumabp@gmail.com>
2014-07-17 17:59:17 +0300

    travis again ;(

--- assets/js/client/app.js
+++ assets/js/client/app.js
@@ -2,7 +2,7 @@ define([
     "jquery",
     "backbone",
     "./routes/main"
-], function ($, Backbone, Router) {
+], function ($, backbone, Router) {
     "use strict";
 
     // http://backbonetutorials.com/organizing-backbone-using-modules/
@@ -11,13 +11,13 @@ define([
     $(document.body).on("click", "a[href]:not([data-bypass])", function (e) {
         if (new RegExp("^((f|ht)tps?:)?//" + location.host).test(this.href)) {
             e.preventDefault();
-            Backbone.history.navigate(this.pathname, true);
+            backbone.history.navigate(this.pathname, true);
         }
     });
 
     return (function () {
         new Router();
         // https://blog.isotoma.com/2014/01/backbone-history-and-ie9/
-        Backbone.history.start({pushState: true});
+        backbone.history.start({pushState: true});
     })();
 });
